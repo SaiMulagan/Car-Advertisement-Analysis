8a6f7f3fc781dbcb28ad4bd4b390b7265a9cb3ab
Sai Mulagan <89549431+SaiMulagan@users.noreply.github.com>
2023-09-24 21:43:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+
+df = pd.read_csv('vehicles_us.csv')
+
+st.header('Car Advertisement Data')
+
+if st.checkbox('Show Price Histogram'):
+    fig = px.histogram(df, x="price", title="Price Distribution")
+    st.plotly_chart(fig)
+
+if st.checkbox('Show Age vs Price Scatter Plot'):
+    fig_scatter = px.scatter(df, x="age", y="price", title="Age vs Price")
+    st.plotly_chart(fig_scatter)
+
+streamlit run app.py
